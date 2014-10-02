a22f7572e360a6591e58457a426d8c5fb830e9cb
eyenseo <eyenseo@gmail.com>
2014-10-02 14:00:29 +1000

    Add signal handling

--- src/server/main.c
+++ src/server/main.c
@@ -1,13 +1,39 @@
+#include <sys/signalfd.h>
+#include <signal.h>
+#include <unistd.h>
+
 #include "server/server.h"
 
 int main(int argc, char const* argv[]) {
   server_t* s;
+  sigset_t mask;
+  siginfo_t info;
+  struct sigaction action;
 
   server_create(&s, "../calories.csv", NULL);
 
-  server_start(s);
-  getchar();
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGINT);
+  sigprocmask(SIG_BLOCK, &mask, NULL);
 
-  server_destroy(s);
-  return 0;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = SA_SIGINFO;
+  sigaction(SIGCHLD, &action, NULL);
+
+  server_start(s);
+
+  while(true) {
+    // signal() is NOT thread safe!
+    // And since the server doesn't block signal handling is "not" needed ...
+    if(sigwaitinfo(&mask, &info) == -1) {
+      perror("sigwaitinfo() failed");
+      exit(1);
+    }
+
+    switch(info.si_signo) {
+    case SIGINT:
+      server_destroy(s);
+      return 0;
+    }
+  }
 }
